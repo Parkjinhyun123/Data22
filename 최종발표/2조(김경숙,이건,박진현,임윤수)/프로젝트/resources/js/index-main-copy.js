b65be2e22ed387bd82e9d4455d52f34368886202
Parkjinhyun123 <pjh1454@naver.com>
2023-12-06 15:50:21 +0900

    폴더정리

--- 최종발표/2조(김경숙,이건,박진현,임윤수)/프로젝트/resources/js/index-main-copy.js
+++
@@ -1,8 +0,0 @@
-const spanTag = document.querySelectorAll(".spanTag");
-
-window.onload = () => {
-  let timer = 100;
-  spanTag.forEach((item) => {
-    item.style.animation = `panimation 500ms ${(timer += 80)}ms forwards`;
-  });
-};
