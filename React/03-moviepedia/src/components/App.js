20e94899864affd74139f44d4e89913aacc92789
Parkjinhyun123 <pjh1454@naver.com>
2023-12-15 12:41:10 +0900

    react

--- React/03-moviepedia/src/components/App.js
+++ React/03-moviepedia/src/components/App.js
@@ -1,11 +1,14 @@
 import { getDatas } from "../firebase";
-import mockItems from "../mock.json";
+// import mockItems from "../mock.json";
 import ReviewList from "./ReviewList";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+
+const LIMIT = 5;
 
 function App() {
   const [items, setItems] = useState([]);
   const [order, setOrder] = useState("createdAt");
+  const [lq, setLq] = useState({});
 
   // sort 함수에 아무런 arguments도 전달하지 않을 때는 기본적으로 유니코드에 정의된 문자열 순서에 따라 정렬된다.
   // ==> compareFunction가 생략될 경우, 배열의 모든 요소들은 문자열 취급되며, 유니코드값 순서대로 정렬된다는 의미이다.
@@ -16,25 +19,21 @@ function App() {
   // a-b : 오름차순, b-a : 내림차순
 
   // const handleNewestClick = () => {
-  //   console.log(item)
+  //   console.log(items);
   //   const sortedItems = items.toSorted((a, b) => b.createdAt - a.createdAt);
+  //   console.log(sortedItems);
   //   setItems(sortedItems);
   // };
 
   // const handleBestClick = () => {
+  //   console.log(items);
   //   const bestItems = items.toSorted((a, b) => b.rating - a.rating);
   //   setItems(bestItems);
   // };
 
-  const sortedItems = items.sort((a, b) => b[order] - a[order]);
-
-  const handleNewestClick = () => {
-    setOrder("createdAt");
-  };
-
-  const handleBestClick = () => {
-    setOrder("rating");
-  };
+  // const sortedItems = items.sort((a, b) => b[order] - a[order]);
+  const handleNewestClick = () => setOrder("createdAt");
+  const handleBestClick = () => setOrder("rating");
 
   const handleDelete = (id) => {
     // items에서 id 파라미터와 같은 id를 가지는 요소(객체)를 제거
@@ -42,9 +41,28 @@ function App() {
     setItems(nextItems);
   };
 
-  const handleLoadClick = async () => {
-    const { reviews } = await getDatas("movie");
+  const handleLoad = async (lq) => {
+    const { reviews, lastQuery } = await getDatas("movie", order, LIMIT, lq);
+    if (lq === undefined) {
+      setItems(reviews);
+    } else {
+      setItems((prevItems) => [...prevItems, ...reviews]);
+    }
+    setLq(lastQuery);
+    console.log(reviews);
   };
+
+  const handleLoadMore = () => {
+    handleLoad(lq);
+  };
+
+  // useEffect는 arguments로 콜백함수와 배열을 넘겨준다.
+  // []은 dependency list 라고 하는데 위에서 handleLoad 함수가 무한루프 작동을 하기 때문에 처리를 해줘야 하는데
+  // react는 [] 안에 있는 값들을 앞에서 기억한 값이랑 비교한다.
+  // 비교해서 다른 경우에만 콜백함수를 실행한다.(그 전에는  콜백함수를 등록만 해놓음)
+  useEffect(() => {
+    handleLoad();
+  }, [order]);
 
   return (
     <div>
@@ -53,7 +71,7 @@ function App() {
         <button onClick={handleBestClick}>베스트순</button>
       </div>
       <ReviewList items={items} onDelete={handleDelete} />
-      <button onClick={handleLoadClick}>불러오기</button>
+      <button onClick={handleLoadMore}>더보기</button>
     </div>
   );
 }
--- React/03-moviepedia/src/firebase.js
+++ React/03-moviepedia/src/firebase.js
@@ -9,27 +9,54 @@ import {
   doc,
   deleteDoc,
   updateDoc,
-  deleteField,
+  query,
+  orderBy,
+  limit,
+  startAfter,
 } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCt_ryGXwrRNWyI7sZL_bjfplQnw21YHsQ",
-  authDomain: "project-03-04-78bcf.firebaseapp.com",
-  projectId: "project-03-04-78bcf",
-  storageBucket: "project-03-04-78bcf.appspot.com",
-  messagingSenderId: "714216538777",
-  appId: "1:714216538777:web:4d109358cbd0891123ee08",
+  apiKey: "AIzaSyBxsGndPgM3bnHcAw41aGjPi-uboA7rauU",
+  authDomain: "asdf-5d972.firebaseapp.com",
+  projectId: "asdf-5d972",
+  storageBucket: "asdf-5d972.appspot.com",
+  messagingSenderId: "450191732725",
+  appId: "1:450191732725:web:8fb7315d1dad6143f6babc",
+  measurementId: "G-SY8XW64PXW",
 };
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
-async function getDatas(collectionName) {
-  const querySnapshot = await getDocs(collection(db, collectionName));
+async function getDatas(collectionName, order, limitNum, lq) {
+  // const querySnapshot = await getDocs(collection(db, collectionName));
+  let docQuery;
+  if (lq === undefined) {
+    docQuery = query(
+      collection(db, collectionName),
+      orderBy(order, "desc"),
+      limit(limitNum)
+      // asc:오름차 순 (굳이 x) desc: 내림차 순
+    );
+  } else {
+    docQuery = query(
+      collection(db, collectionName),
+      orderBy(order, "desc"),
+      startAfter(lq),
+      limit(limitNum)
+      // asc:오름차 순 (굳이 x) desc: 내림차 순
+    );
+  }
+  const querySnapshot = await getDocs(docQuery);
+  // 쿼리 query
+  // orderBy, limit, startAfter
   const result = querySnapshot.docs;
+  const lastQuery = result[result.length - 1];
   const reviews = result.map((doc) => doc.data());
-  console.log(reviews);
-  return { reviews };
+
+  const option = "";
+
+  return { reviews, lastQuery };
 }
 
 export {
@@ -44,5 +71,4 @@ export {
   // addDatas,
   deleteDoc,
   updateDoc,
-  deleteField,
 };
